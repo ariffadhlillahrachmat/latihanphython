3fe680a08395e78465ca7ad425eaa8174779c7ab
ariffadhlillahrachmat <147135806+ariffadhlillahrachmat@users.noreply.github.com>
2023-10-07 10:22:37 +0700

    Add files via upload
    pak ini ketinggalan

---
+++ latihan2.py
@@ -0,0 +1,29 @@
+kalimat = "ini adalah sebuah kalimat"
+paragraf = """Ini adalah paragraf.
+paragraf terdiri dari beberapa baris."""
+print(kalimat)
+print(paragraf)
+
+kalimat = "abcdefghijklmnopqtrtrefehegffdggtrhfdfd"
+print(kalimat[:5])
+print(kalimat[5:10])
+print(kalimat[21:])
+print(kalimat[-10:-1])
+print(kalimat[-10:])
+
+
+kalimat = "Hallo, semua. Saya adalah adalah seorang programer python (amiinn)"
+print(kalimat.upper())
+print(kalimat.lower())
+print(kalimat.capitalize())
+print(kalimat.title())
+print(kalimat.swapcase())
+print(kalimat.replace("Python", "java"))
+print(kalimat.replace("a", "i"))
+print(kalimat.strip())
+print(kalimat.split('.'))
+
+
+kalimat = "Hallo, semua saya adalah seorang programer python"
+tambahan = "Saya sedang belajar \"python\". Mohon bimbingannya."
+print(kalimat + tambahan)
